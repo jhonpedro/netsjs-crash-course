e710070a97a430cde4c5638cc1afca2f7df77ce0
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-17 13:53:46 -0300

    docs: add swagger api property

--- src/entities/plants.entity.ts
+++ src/entities/plants.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   Entity,
@@ -9,18 +10,23 @@ import { User } from './user.entity';
 
 @Entity({ name: 'plants' })
 export class Plant {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({ name: 'user_id' })
   userId: number;
 
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty()
   @Column({ name: 'specie_name' })
   specieName: string;
 
+  @ApiProperty({ example: 'An user' })
   @ManyToOne(() => User, (user) => user.plants)
   @JoinColumn({ name: 'user_id' })
   user: User;
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   Entity,
@@ -10,11 +11,14 @@ import { Plant } from './plants.entity';
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty({ example: ['An array full of plants'] })
   @OneToMany(() => Plant, (plant) => plant.user)
   @JoinColumn({ name: 'user_id' })
   plants: Plant[];
