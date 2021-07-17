11d253c84573e1fd61e549f688579234a288c508
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-17 15:41:34 -0300

    feat: add typeorm, config and plants module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { PlantsModule } from './plants/plants.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PlantsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+  ],
 })
 export class AppModule {}
